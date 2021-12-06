e437f24d147a24ce14e054ab1d86beaf65b013cf
kimseokjin <kimseokjin0324@gmail.com>
2021-12-06 22:43:39 +0900

    login 쿠키를 이용한 로그인 유지, but 쿠키를 이용하면 보안상 엄청나게 위험이있다.

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -21,28 +23,44 @@ public class LoginController {
 
 
     @GetMapping("/login")
-    public String loginForm(@ModelAttribute("loginForm")LoginForm form){
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "login/loginForm";
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
 
         Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
 
         //-글로벌 오류
-        if(loginMember==null){
-            bindingResult.reject("loginFail"," 아이디 또는 비밀번호가 맞지 않습니다.");
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", " 아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
-        //로그인 성공 처리 TODO
+        //로그인 성공 처리
 
+        //쿠키생성
+        //쿠키에 시간 정보를 주지않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        //로그인 성공시 쿠키생성 HttpServletResponse에 담고 쿠키이름은 memberId이고 값은 회원의 id이다.
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
         return "redirect:/";
     }
 
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse respose){
+        expireCookie(respose, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse respose, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        respose.addCookie(cookie);
+    }
 
 
 }
